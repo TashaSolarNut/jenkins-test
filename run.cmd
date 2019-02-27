0376db984d0eb98fe9af02128c86379f4f507a3e
Igor Akulov <egor@akulovs.com>
2019-02-28 01:48:40 +0700

    simple

--- run.cmd
+++ run.cmd
@@ -1,1 +1,1 @@
-mvn clean test -Dwebdriver.chrome.driver=C:\opt\src\Testing\drivers\chromedriver.exe -Dwebdriver.gecko.driver=C:\opt\src\Testing\drivers\geckodriver.exe
+mvn clean test -Dbrowser=firefox -Dwebdriver.chrome.driver=C:\opt\src\Testing\drivers\chromedriver.exe -Dwebdriver.gecko.driver=C:\opt\src\Testing\drivers\geckodriver.exe
